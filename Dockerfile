f99aafcaedd8949f715a2c440f2e6e38f3b7b9df
Jonas Kunz <jonas.kunz@elastic.co>
2024-10-09 11:14:30 +0200

    Replaced with test-case to reproduce linux-vdso.1.so stack unwinding problem

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,9 @@ RUN ./mvnw package
 ARG ELASTIC_OTEL_JAVAAGENT_VERSION=1.0.0
 RUN wget --tries=5 -qO- https://repo1.maven.org/maven2/co/elastic/otel/elastic-otel-javaagent/${ELASTIC_OTEL_JAVAAGENT_VERSION}/elastic-otel-javaagent-${ELASTIC_OTEL_JAVAAGENT_VERSION}.jar > ./elastic-otel-javaagent.jar
 
-FROM eclipse-temurin:21-jre-ubi9-minimal
+
+FROM ubuntu:jammy@sha256:7c75ab2b0567edbb9d4834a2c51e462ebd709740d1f2c40bcd23c56e974fe2a8
+RUN apt update && apt -y install openjdk-21-dbg
 
 COPY --from=build /build/target/cpu-burner.jar /
 COPY --from=build /build/elastic-otel-javaagent.jar /
@@ -20,4 +22,4 @@ COPY --from=build /build/elastic-otel-javaagent.jar /
 ENV OTEL_SERVICE_NAME=cpu-burner-otel
 ENV ELASTIC_OTEL_UNIVERSAL_PROFILING_INTEGRATION_ENABLED=true
 
-CMD ["java","-javaagent:./elastic-otel-javaagent.jar", "-jar", "./cpu-burner.jar"]
+CMD ["java","-jar", "./cpu-burner.jar"]
--- src/main/java/co/elastic/demo/CpuBurnerMain.java
+++ src/main/java/co/elastic/demo/CpuBurnerMain.java
@@ -4,7 +4,6 @@ import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.instrumentation.annotations.WithSpan;
-import org.bouncycastle.jcajce.provider.asymmetric.rsa.DigestSignatureSpi;
 import org.bouncycastle.jcajce.provider.digest.SHA3;
 
 import java.security.MessageDigest;
@@ -13,127 +12,11 @@ import java.util.Random;
 
 public class CpuBurnerMain {
 
-    private static final Tracer tracer = GlobalOpenTelemetry.getTracer("manual-tracer");
-    public static volatile Object memoryDrain;
-
-    public static volatile Object sink;
 
     public static void main(String[] args) throws InterruptedException, NoSuchAlgorithmException {
-
-        MessageDigest[] hashAlgos = new MessageDigest[] {
-          MessageDigest.getInstance("SHA-256"),
-          new SHA3.DigestSHA3(256)
-        };
-
-
-        Thread t1 = new Thread(() -> {
-            while (true) {
-                try {
-                    fibonacciFun();
-                } catch (Exception e) {}
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    System.out.println("Stopping fibonacci transactions due to interrupt");
-                    break;
-                }
-            }
-        });
-
-
-        Thread t2 = new Thread(() -> {
-            int iteration = 0;
-            while (true) {
-                iteration++;
-                shaShenanigans(hashAlgos[iteration % hashAlgos.length]);
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    System.out.println("Stopping sha transactions due to interrupt");
-                    break;
-                }
-            }
-        });
-
-        Thread t3 = new Thread(() -> {
-            while (true) {
-                try {
-                    backgroundAllocate();
-                } catch (Exception e) {}
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    break;
-                }
-            }
-        });
-
-        Thread[] threads = {
-                t1,
-                t2,
-                t3
-        };
-        for (Thread thread : threads) {
-            thread.start();
-        }
-        for (Thread thread : threads) {
-            thread.join();
-        }
-    }
-
-    private static void backgroundAllocate() {
-        for (int i=0; i<200; i++) {
-            memoryDrain = new byte[512*1024];
-        }
-    }
-
-    public static void fibonacciFun() {
-        Span span = tracer.spanBuilder("fibonacciFun").startSpan();
-        try (var scope = span.makeCurrent()) {
-            long start = System.nanoTime();
-            while ((System.nanoTime() - start) < 100_000_000L) {
-                sink = computeFibonacci();
-            }
-        } finally {
-            span.end();
-        }
-    }
-
-    public static void shaShenanigans(MessageDigest digest) {
-        Span span = tracer.spanBuilder("shaShenanigans")
-                .setAttribute("algorithm", digest.getAlgorithm())
-                .startSpan();
-
-        try (var scope = span.makeCurrent()) {
-            long start = System.nanoTime();
-            while ((System.nanoTime() - start) < 100_000_000L) {
-                sink = hashRandomStuff(digest);
-            }
-        } finally {
-            span.end();
-        }
-    }
-
-    @WithSpan
-    private static byte[] hashRandomStuff(MessageDigest digest) {
-        Random rnd = new Random();
-        byte[] buffer = new byte[1024];
-        for (int i = 0; i < 5000; i++) {
-            rnd.nextBytes(buffer);
-            digest.update(buffer);
-        }
-        return digest.digest();
+        CPULoad.getInstance().execute(true);
+        Thread.sleep(999999999);
     }
 
 
-    @WithSpan
-    private static long computeFibonacci() {
-        return fibonacci(35);
-    }
-
-    private static long fibonacci(long n) {
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        return fibonacci(n-1) + fibonacci(n - 2);
-    }
 }
